879f8e154b3839f66e8f51417dab7f453fc6f210
viktor <sneik_11@mail.ru>
2020-03-13 17:53:46 +0600

    helvetica and roboto fonts, Roboto font default

--- app/scripts/lib/sayHello.js
+++ app/scripts/lib/sayHello.js
@@ -1,9 +1,9 @@
 function sayHello() {
     if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-        var args = ['\n %c Made with ❤️ by Amphibia %c http://www.amphibia.kz %c %c \n', 'border: 1px solid #284ECB;color: #fff; background: #284ECB; padding:5px 0;', 'color: #fff; padding:5px 0;border: 1px solid #284ECB;', 'padding:5px 0;', 'color: #b0976d; padding:5px 0;'];
+        var args = ['\n %c Made with ❤️ by Maint %c https://maint.kz %c %c \n', 'border: 1px solid #284ECB;color: #fff; background: #284ECB; padding:5px 0;', 'color: #fff; padding:5px 0;border: 1px solid #284ECB;', 'padding:5px 0;', 'color: #b0976d; padding:5px 0;'];
         window.console.log.apply(console, args);
     } else if (window.console) {
-        window.console.log('Made with love ❤️ Amphibia.kz - http://www.amphibia.kz/  ❤️');
+        window.console.log('Made with love ❤️ Maint.kz - https://maint.kz/  ❤️');
     }
 };
 
