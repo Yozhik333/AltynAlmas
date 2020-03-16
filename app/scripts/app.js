1a1d10c7ecc383f397a5760ec529a705315dd240
viktor <sneik_11@mail.ru>
2020-03-16 15:54:58 +0600

    общие стили для article, стили блока Наши приимущества, класс .js-text-ellipsis для обрезки текста в 270символов

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,21 @@
 import sayHello from './lib/sayHello';
 import {mainTopSlider} from './lib/mainTopSlider';
+
 sayHello();
+
+(function () {
+  
+  const cropElement = document.querySelectorAll('.js-text-ellipsis'); // выбор элементов
+  const size = 270;                                   // кол-во символов
+  const endCharacter = '...';                                  // окончание
+  
+  cropElement.forEach(el => {
+    let text = el.innerHTML;
+    
+    if (el.innerHTML.length > size) {
+      text = text.substr(0, size);
+      el.innerHTML = text + endCharacter;
+    }
+  });
+  
+}());
