b21b4ec8bc05336ba38c8c680aea3c82909ed5ca
Yozhik333 <repeinik333@gmail.com>
2020-03-18 16:39:32 +0600

    Не пойму почему не работает Jquery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,3 +24,8 @@ sayHello();
   
 }());
 
+$(".tab_item").not(":first").hide();
+$(".tabs-wrapper .tab").click(function() {
+	$(".tabs-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
