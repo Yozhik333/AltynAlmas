3ae1184d38c9abe98c2b61bef92e340ff1f5b28c
viktor <sneik_11@mail.ru>
2020-04-01 18:47:16 +0600

    карта

---
+++ app/scripts/lib/map.js
@@ -0,0 +1,22 @@
+const map = document.querySelector('#map');
+if (map) {
+  ymaps.ready(function () {
+    let myMap = new ymaps.Map('map', {
+        center: [43.238772,76.943561],
+        zoom: 16.3
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+      
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Altyn Almas'
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/samples/pin.svg',
+        iconImageSize: [60, 82],
+        iconImageOffset: [-30, -90]
+      });
+    myMap.geoObjects
+      .add(myPlacemark);
+  });
+}
