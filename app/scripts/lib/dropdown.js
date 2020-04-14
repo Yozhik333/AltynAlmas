4cba58ef981471b4fbd0cc4663bbcd2ad6f91c87
Viktor <sneik_11@mail.ru>
2020-04-14 13:55:26 +0600

    мобильное меню

--- app/scripts/lib/dropdown.js
+++ app/scripts/lib/dropdown.js
@@ -1,34 +1,22 @@
 // FOR DROPDOWN
 const dropdownLink = document.querySelectorAll('.dropdownWrap');
-const test = document.querySelectorAll('.dropdownWrap > .link')
 dropdownLink.forEach(item => {
-  item.addEventListener("mouseenter", function (event) {
-    this.classList.add('dropdownShow')
-    // backToMainMenu.style.display = "flex";
-    // backToMainMenu.addEventListener('click', function () {
-    //   this.classList.remove('dropdownShow')
-    // })
-  }, false);
-  item.addEventListener("mouseleave", function (event) {
-    this.classList.remove('dropdownShow')
-    // backToMainMenu.style.display = "none";
-  }, false);
+    item.addEventListener("mouseenter", function (event) {
+        this.classList.add('dropdownShow')
+    }, false);
+    item.addEventListener("mouseleave", function (event) {
+        this.classList.remove('dropdownShow')
+    }, false);
 })
 
-let prevDev = document.querySelectorAll('.prevDev')
-
-let mediaQuery = x => {
-  if (x.matches) {
-    prevDev.forEach(item => {
-      item.addEventListener('click', function (event) {
-        event.preventDefault();
-      })
+let mobileMenu = document.querySelector('.mobile-menu ul')
+mobileMenu.addEventListener('click', function (e) {
+    const items = document.querySelectorAll('.toggle')
+    const target = e.target
+    Array.from(items).forEach(item => {
+        item.classList.remove('active')
     })
-  }
-}
 
-let x = window.matchMedia("(max-width: 992px)")
-mediaQuery(x)
-x.addListener(mediaQuery)
+    target.classList.add('active')
+})
 
-console.log(test)
