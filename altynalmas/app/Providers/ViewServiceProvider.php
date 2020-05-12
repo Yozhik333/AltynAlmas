78ad23dd945d12748654129a5584db2c6a0f757b
Ozymandiaq <alexey.stolyarov817@gmail.com>
2020-05-12 21:57:01 +0600

    промежуточный коммит по добавлению ссылки на hh в меню altynalmas

--- altynalmas/app/Providers/ViewServiceProvider.php
+++ altynalmas/app/Providers/ViewServiceProvider.php
@@ -32,13 +32,13 @@ class ViewServiceProvider extends ServiceProvider
         view()->share('usd', $usd );
 
 
-        $subject = file_get_contents('https://www.kitco.com/');
-        $pattern = '/<tr id="row-USD" class="row">  <td id="lgq-bid">(.+?)<\/td><td id="lgq-ask">(.+?)<\/td>    <td class="red" id="lgq-chg">(.+?)<\/td>    <td class="red" id="lgq-chg-percent">(.+?)<\/td>    <\/tr>/u';
-        $result = [];
-        preg_match_all($pattern, $subject, $result);
-        $usd = $result[1][0];
+        // $subject = file_get_contents('https://www.kitco.com/');
+        // $pattern = '/<tr id="row-USD" class="row">  <td id="lgq-bid">(.+?)<\/td><td id="lgq-ask">(.+?)<\/td>    <td class="red" id="lgq-chg">(.+?)<\/td>    <td class="red" id="lgq-chg-percent">(.+?)<\/td>    <\/tr>/u';
+        // $result = [];
+        // preg_match_all($pattern, $subject, $result);
+        // $usd = $result[1][0];
         
-        view()->share('usd', $usd );
+        // view()->share('usd', $usd );
 
 
     }
