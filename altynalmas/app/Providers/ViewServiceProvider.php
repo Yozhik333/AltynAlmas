c4f0fefeaffaaea02300fbb925c8930b39dde7b4
Ozymandiaq <alexey.stolyarov817@gmail.com>
2020-06-01 11:22:39 +0600

    временно убрал автоматическое обновление курсов, проблемы с доступом на сайт нацбанка

--- altynalmas/app/Providers/ViewServiceProvider.php
+++ altynalmas/app/Providers/ViewServiceProvider.php
@@ -23,13 +23,13 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $subject = file_get_contents('https://nationalbank.kz/rss/get_rates.cfm');
-        $pattern = '/<title>USD<\/title><description>(.+?)<\/description>/u';
-        $result = [];
-        preg_match_all($pattern, $subject, $result);
-        $usd = $result[1][0];
+        // $subject = file_get_contents('https://nationalbank.kz/rss/get_rates.cfm');
+        // $pattern = '/<title>USD<\/title><description>(.+?)<\/description>/u';
+        // $result = [];
+        // preg_match_all($pattern, $subject, $result);
+        // $usd = $result[1][0];
         
-        view()->share('usd', $usd );
+        // view()->share('usd', $usd );
 
 
         // $subject = file_get_contents('https://www.kitco.com/');
